066ed8c6b1e0ef15796e8bffa512ac130d5e5541
Borusiewicz <tomasz.borusiewicz@nokia.com>
2020-04-15 17:57:09 +0200

    Aktualizacja pracy

--- moduly_kod/Xbee_nadajnik/Xbee_nadajnik.ino
+++ moduly_kod/Xbee_nadajnik/Xbee_nadajnik.ino
@@ -1,14 +1,8 @@
-const int potPin = A0;
-int value ;
-
 void setup() {
-	Serial.begin(9600);
+	Serial.begin(9600); // Zainicjalizuj serial z baudrate = 9600
 }
 
 void loop() {
-  	value = analogRead(A0);
-  	value = map (value, 0, 1023, 0, 255);
-	Serial.print('<');
- 	Serial.print(value);
-	Serial.println('>');
+ 	Serial.println("Hello, world!"); // Wyślij na interfejs serialowy wiadomośc
+	delay(2000); // poczekaj 2 sekundy
 }
--- moduly_kod/Xbee_odbiornik/Xbee_odbiornik.ino
+++ moduly_kod/Xbee_odbiornik/Xbee_odbiornik.ino
@@ -1,37 +1,9 @@
-bool started= false;
-bool ended 	= false;
-char incomingByte ; 
-char msg[3];		
-byte index;			
-
 void setup() {
-
-	Serial.begin(9600);
+	Serial.begin(9600); // Zainicjalizuj serial z baudrate = 9600
 }
 
 void loop() {
-	
-  while (Serial.available()>0){
-    incomingByte = Serial.read();
-    if(incomingByte == '<')
-    {
-     started = true;
-     index = 0;
-     msg[index] = '\0';
+ 	if (Serial.available() > 0){ // Jeżeli serial jest dostępny
+     Serial.write(Serial.read()); // Wyświetl na interfejsie serialowym to co odbiornik otrzymał od nadajnika
    }
-   else if(incomingByte == '>')
-   {
-     ended = true;
-     break;
-   }
-   else
-   {
-     if(index < 4)
-     {
-       msg[index] = incomingByte;
-       index++;
-       msg[index] = '\0';
-     }
-   }
-  }
 }
